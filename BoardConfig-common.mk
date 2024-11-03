9d67bdb763543001e826eb4a6813f76af03105f2
Pranav Vashi <neobuddy89@gmail.com>
2024-11-03 15:29:35 +0530

    zumapro: Update arch setup
    Change-Id: Ia956fd429c9c3ba4842995e31d259d5ac1a587b3
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -14,9 +14,10 @@ include device/google/zumapro/BoardConfig-16k-common.mk
 endif
 
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_VARIANT := cortex-a55
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a510
 
 BOARD_BOOTCONFIG += \
     androidboot.boot_devices=13200000.ufs
