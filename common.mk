35ca8e11d04403e9fab6dac0d45cd77afaaf98a4
Pranav Vashi <neobuddy89@gmail.com>
2024-11-11 08:39:36 +0530

    zumapro: Enable UDFPS icons and animations
    Change-Id: Ie6ec8bd6d1ca8a35c2a11348f9e452100844281d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -365,6 +365,8 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product-if-exists, vendor/google/faceunlock/config.mk)
 
 # Fingerprint
+TARGET_HAS_UDFPS := true
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
