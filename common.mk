3cb96e542747fcda72b0d6d46db23d03ed4cbd56
Ionut Gherman <ghermanionut96@gmail.com>
2024-12-01 12:03:19 +0200

    zumapro: Add require stronger taps
    * Fixes system_server and hardware sensors multihal high CPU when Quick Tap is enabled
    
    Signed-off-by: Ionut Gherman <ghermanionut96@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -436,6 +436,10 @@ TARGET_PRODUCT_PROP += device/google/zumapro/product.prop
 TARGET_SYSTEM_EXT_PROP += device/google/zumapro/system_ext.prop
 TARGET_VENDOR_PROP += device/google/zumapro/vendor.prop
 
+# Quick Tap
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.columbus.use_ap_sensor=false
+
 # SecureElement
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
